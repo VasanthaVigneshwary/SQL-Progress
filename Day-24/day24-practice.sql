fdd190f8285a742892aad5b77ea7bd95c4117056
VarshaMurugadhass <varshascrafty@gmail.com>
2026-07-23 21:46:03 +0530

    Updated README

---
+++ Day-24/day24-practice.sql
@@ -0,0 +1,33 @@
+# 🚀 SQL Day 23 – SQL Constraints (Introduction)
+
+## 📖 Topics Covered
+- Introduction to SQL Constraints
+- PRIMARY KEY
+- NOT NULL
+- UNIQUE
+- DEFAULT
+- CHECK
+- FOREIGN KEY (Introduction)
+- Difference between PRIMARY KEY and UNIQUE
+- Constraint Validation
+- Constraint Violations
+
+## 💻 Concepts Practiced
+- Creating tables with constraints
+- Inserting valid records
+- Understanding constraint violations
+- Using DEFAULT values
+- Viewing table structure using DESC
+
+## 📂 Files
+- `day23_practice.sql`
+- `notes.md`
+
+## 🎯 Outcome
+After completing Day 23, I can:
+- Understand why SQL Constraints are used.
+- Create tables using different constraints.
+- Differentiate between PRIMARY KEY and UNIQUE.
+- Use NOT NULL, DEFAULT, and CHECK constraints.
+- Understand the purpose of FOREIGN KEY.
+- Predict errors caused by constraint violations.
